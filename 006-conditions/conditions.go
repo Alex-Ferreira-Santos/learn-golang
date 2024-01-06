76a9f8e713646723023dad37ffa585b5dd33b493
Alex Ferreira Santos <alexfstos@gmail.com>
2024-01-06 10:26:12 -0300

    feat: conditions

---
+++ 006-conditions/conditions.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 4
+	y := 5
+	fmt.Println("x < y", x < y)
+	fmt.Println("x > y", x > y)
+	fmt.Println("x <= y", x <= y)
+	fmt.Println("x >= y", x >= y)
+	fmt.Println("x == y", x == y)
+	fmt.Println("x != y", x != y)
+}
---
+++ 006-conditions/conditions.md
@@ -0,0 +1,8 @@
+Conditions operations:
+
+< - lower than
+> - bigger than
+>= - bigger than or equal
+<= - lower than or equal
+== - equal
+!= - different
